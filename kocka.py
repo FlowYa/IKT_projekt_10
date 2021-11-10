8cce783c0f05ac151d25fb89b341d35133641841
Flow <90308447+FlowYa@users.noreply.github.com>
2021-11-10 10:30:29 +0100

    Add files via upload

---
+++ kocka.py
@@ -0,0 +1,79 @@
+import os
+import time
+import random
+rand = random.randint
+def clear():
+    os.system('CLS')
+for dobas in range(1,7):
+   if dobas == 1: 
+       print(r"""
+   
+   
+           _______            
+         /\       \           
+        /o \   o   \          
+       /    \_______\         
+       \    /o      /         
+        \ o/  o    /          
+         \/___ _o / """)
+  
+   elif dobas == 2: 
+         print(r"""   
+
+             _______            
+         /\   o   \           
+        /o \  o    \          
+       /    \_______\         
+       \    /o      /         
+        \ o/  o    /          
+         \/___ _o / """)
+  
+   elif dobas == 3: 
+         print(r"""   
+
+
+          _______            
+         /\ o     \           
+        /o \   o   \          
+       /    \____o__\         
+       \    /o   o  /         
+        \ o/       /          
+         \/_o_ _o / """)
+  
+   elif dobas == 4: 
+        print(r"""   
+
+
+          _______            
+         /\ o  o \           
+        /o \      \       
+       /    \_o__o_\         
+       \    /o      /         
+        \ o/  o    /          
+         \/___ _o / """)
+
+   elif dobas == 5: 
+        print(r"""   
+
+
+          _______            
+         /\ o   o \           
+        /o \   o   \          
+       /    \_o  o__\         
+       \    /o      /         
+        \ o/  o    /          
+         \/___ _o / """)
+  
+   elif dobas == 6: 
+        print(r"""   
+
+
+          _______            
+         /\ o o o \           
+        /o \ o o o \          
+       /    \_______\         
+       \    /o      /         
+        \ o/  o    /          
+         \/___ _o / """)
+  
+       
